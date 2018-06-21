37db431e9d788de73695eb5a777a778397ce3032
Christopher Petersen <chris@petersen.io>
2018-06-20 20:22:37 -0700

    Find files in any parent directory

--- lib/git-trim.rb
+++ lib/git-trim.rb
@@ -7,7 +7,6 @@ class GitTrim
     if File.exists?(".git-protected-branches")
       protected_branches = File.read(".git-protected-branches").split("\n")
     end
-    protected_branches ||= []
     protected_branches << "master"
 
     branches = %x{git branch --merged master}.split("\n").collect {|b| b.gsub('*', '').strip}
@@ -15,6 +14,17 @@ class GitTrim
 
     branches.each do |branch|
       puts %x{git branch -d #{branch}}
+    end
+  end
+
+  def find_file(filename, directory=Dir.pwd)
+    local_filename = File.expand_path(filename, directory)
+    if File.exists?(local_filename)
+      return local_filename
+    elsif directory == "/"
+      return nil
+    else
+      return find_file(filename, File.expand_path("..", directory))
     end
   end
 end
